41e4bda4a2e4b6696071fabf9a8298eb3ec14629
DESKTOP-3UJG850\sungyoun <baebull70@gmail.com>
2023-07-09 00:10:20 +0900

    initial commit

---
+++ src/main/java/com/flab/fpay/FpayApplication.java
@@ -0,0 +1,13 @@
+package com.flab.fpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FpayApplication.class,args);
+    }
+
+}
